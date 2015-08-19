ce51d96c6f661f9c5ac934aed1927cdf873d7ff3
hilpitome <hilpitome@gmail.com>
2015-08-19 21:26:58 +0300

    mote setup complete

--- app.rb
+++ app.rb
@@ -1,13 +1,11 @@
 require "cuba"
+require "mote"
+require "mote/render"
+
+Cuba.plugin(Mote::Render)
 
 Cuba.define do
 	on root do
-	    res.write("
-      <html>
-        <body>
-          <!-- ... -->
-        </body>
-      </html>
-    ")
+	    res.write("Hello Frogger!")
 	  end
 end
